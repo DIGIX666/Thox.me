79e6d0c64e9f083feef371cf4ecfb1c7921a5278
DIGIX666 <digixit66@gmail.com>
2026-02-03 18:25:29 +0100

    feat: delet title and modified picture size

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -4,7 +4,7 @@ export default function BlogLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className="w-full max-w-xl lg:max-w-4xl mx-auto px-2 sm:px-4 overflow-x-hidden">
+    <div className="w-full max-w-2xl lg:max-w-4xl mx-auto px-2 sm:px-4 overflow-x-hidden">
       {children}
     </div>
   )
--- app/layout.tsx
+++ app/layout.tsx
@@ -79,7 +79,7 @@ export default function RootLayout({
           amiko.variable
         )}
       >
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto min-h-screen relative">
+      <body className="antialiased max-w-4xl mx-4 mt-8 lg:mx-auto min-h-screen relative">
         <ClientLayout>
           <Stars quantity={100} />
           <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0 relative z-10">
